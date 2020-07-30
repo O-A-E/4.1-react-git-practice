811f176a3c09ebb40f51f1872228dcc4e32b9faf
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-07-30 11:04:23 -0600

    Add Services component and content.

---
+++ src/components/Services.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Services extends React.Component
+{
+    render()
+    {
+        return (
+            <>
+                <p> We offer many great services! Like...</p>
+                <ul>
+                    <li>Project Management</li>
+                    <li>Web Design</li>
+                    <li>Web Development</li>
+                </ul>
+            </>
+        )
+    }
+}
+
+export default Services;
