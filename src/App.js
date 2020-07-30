92eb365106a5a28592ecd3a9fede86e2cbcc3465
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-07-30 10:48:59 -0600

    add content to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
-import './App.css';
+import "./App.css";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <h1> Git Branching/Merge Practice</h1>
       <p> Let's practice!</p>
+      <About />
     </div>
   );
 }
